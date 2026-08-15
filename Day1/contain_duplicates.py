5a6e714773417957988ff5cd7d1e4f35bde0b45d
Harsh Khapekar <harshkhapekar1@gmail.com>
2026-08-15 18:40:43 +0530

    Delete Day1/contain_duplicates.py

--- Day1/contain_duplicates.py
+++
@@ -1,9 +0,0 @@
-def duplicates(arr):
-    seen = set()
-    for ele in arr:
-        if ele  in seen:
-            return True
-        else :
-            seen.add(ele)
-    return False
-print(duplicates([1,2,3,4,1]))
